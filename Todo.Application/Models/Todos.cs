95473ad21a3a3544a12df95aaea2f4a0bd7e9a43
tsxepo <tsxepo@gmail.com>
2023-06-24 01:24:09 +0200

    AddModels

--- Todo.Application/Models/Todos.cs
+++ Todo.Application/Models/Todos.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Todo.Application.Models
 {
     public class Todos
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonId]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
